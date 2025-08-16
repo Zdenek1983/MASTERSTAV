1a680aad34e999813b010decbef5e6597e292029
Zdenek1983 <czaspy@gmail.com>
2025-08-16 19:38:52 +0200

    Updated from Naklikam.cz

--- src/components/admin/reference-management.tsx
+++ src/components/admin/reference-management.tsx
@@ -11,7 +11,7 @@ import {
   Building,
   Image as ImageIcon
 } from 'lucide-react'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 
 interface Reference {
